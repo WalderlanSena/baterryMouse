8600892c0f39ede5e95e837d3e4d50e5f3eb0356
Walderlan Sena <senawalderlan@gmail.com>
2018-03-24 02:11:13 -0300

    bugfix: Corrigido erros gerais do funcionamento do script

--- batteryMouse.sh
+++ batteryMouse.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 #   Script to check if your battery has already been charged
 #   @author: Walderlan Sena <walderlan@mentesvirtuaissena.com>
-#   Code: https://www.github.com/walderlansena
+#   Code: https://www.github.com/walderlansena/
 #   Dependencies: acpi - yad
 #   sudo apt-get install acpi && sudo apt-get install yad
 
-valueCurrent=$(acpi -b | cut -d " " -f 4 | sed 's/,//g' | sed 's/%//g')
-
 while true;
 do
-    sleep 180
+    sleep 60
+    valueCurrent=$(acpi -b | cut -d " " -f 4 | sed 's/,//g' | sed 's/%//g')
     if [ "$valueCurrent" == "100" ];
     then
         yad --center                        \
         --title="Battery Mouse"             \
-        --text="Bateria 100% Carregada"     \
+        --image="baterriy.png"              \
+        --image "dialog-info"               \
+        --button=gtk-ok                     \
+        --text="100% Charged Battery"       \
         --width=300                         \
         --height=100                        \
         --text-align=center                 \
         --window-icon="baterry.png"         
     fi
 done
-
